cb72810069d88a7ef2fdc8b0a1d8a857266d4609
Astr0Dev <abndt2010@gmail.com>
2022-12-16 11:25:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+//selectors
+const startButton = document.getElementById("startButton");
+const timeDisplay = document.getElementById("timeDisplay");
+
+//variables
+let startTime;
+let endTime;
+let totalTime = 0;
+let numTries = 0;
+let gameStarted = false;
+let tries = 5;
+
+//event listener
+startButton.addEventListener("click", () => {
+  if (gameStarted) {
+    // Record end time and update time display
+    if (startButton.innerHTML === "Click now") {
+      endTime = Date.now();
+      const elapsedTime = endTime - startTime;
+      timeDisplay.innerHTML = numTries + 1 + ": " + elapsedTime + "ms";
+      totalTime += elapsedTime;
+      numTries++;
+
+      if (numTries === tries) {
+        const averageTime = totalTime / numTries;
+        timeDisplay.innerHTML = `Average time: ${averageTime.toFixed(2)}ms`;
+      }
+    } else {
+      // Show "too early" message
+      timeDisplay.innerHTML = "Too early, 500ms penalty!";
+      totalTime += 500;
+    }
+
+    // Reset game
+    if (startButton.innerHTML === "Click now") {
+      gameStarted = false;
+      startButton.innerHTML = "START";
+      startButton.style.backgroundColor = "white";
+    }
+  } else {
+    // Set timeout and update button text and background color
+    gameStarted = true;
+    startButton.innerHTML = "In progress...";
+    startButton.style.backgroundColor = "#F0706A";
+
+    setTimeout(() => {
+      startButton.innerHTML = "Click now";
+      startButton.style.backgroundColor = "#ACD1AF";
+      startTime = Date.now();
+    }, 1000 + Math.random() * 1000);
+  }
+});
